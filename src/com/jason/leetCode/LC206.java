a3cecb47b21a8c1383325e041e8a55be0510e589
JasonC5 <chenj@migrsoft.com>
2021-08-17 23:02:16 +0800

    力扣高频

---
+++ src/com/jason/leetCode/LC206.java
@@ -0,0 +1,60 @@
+package com.jason.leetCode;
+
+/**
+ * 你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+ *
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：head = [1,2,3,4,5]
+ * 输出：[5,4,3,2,1]
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：head = [1,2]
+ * 输出：[2,1]
+ * 示例 3：
+ * <p>
+ * 输入：head = []
+ * 输出：[]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author JasonC5
+ */
+public class LC206 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
---
+++ src/com/jason/leetCode/LC230.java
@@ -0,0 +1,52 @@
+package com.jason.leetCode;
+
+/**
+ * 给定一个二叉搜索树的根节点 root ，和一个整数 k ，请你设计一个算法查找其中第k个最小元素（从 1 开始计数）。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：root = [3,1,4,null,2], k = 1
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：root = [5,3,6,2,4,null,null,1], k = 3
+ * 输出：3
+ * <p>
+ * <p>
+ * <p>
+ * <p>
+ * 提示：
+ * <p>
+ * 树中的节点数为 n 。
+ * 1 <= k <= n <= 104
+ * 0 <= Node.val <= 104
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author JasonC5
+ */
+public class LC230 {
+    int k;
+
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        return process(root).val;
+    }
+
+    private TreeNode process(TreeNode node) {
+        if (node == null) {
+            return null;
+        }
+        TreeNode ans = process(node.left);
+        if (ans != null) {
+            return ans;
+        }
+        return --k == 0 ? node : process(node.right);
+    }
+}
