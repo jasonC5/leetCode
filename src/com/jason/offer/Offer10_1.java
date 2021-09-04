648b485d12da9a0dd7c55c5f32d427278110e7d1
JasonC5 <chenj@migrsoft.com>
2021-09-04 09:40:30 +0800

    每日一题

---
+++ src/com/jason/offer/Offer10_1.java
@@ -0,0 +1,60 @@
+package com.jason.offer;
+
+/**
+ * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+ * <p>
+ * F(0) = 0,   F(1) = 1
+ * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+ * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+ * <p>
+ * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 2
+ * 输出：1
+ * 示例 2：
+ * <p>
+ * 输入：n = 5
+ * 输出：5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author JasonC5
+ */
+public class Offer10_1 {
+    public static final int MASK = 1000000007;
+
+    public static int fib0(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return (fib0(n - 1) + fib0(n - 2)) % MASK;
+    }
+
+    public int fib1(int n) {
+        if (n < 1) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MASK;
+        }
+        return dp[n];
+    }
+
+
+}
