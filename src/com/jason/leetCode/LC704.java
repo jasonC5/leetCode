766e8d71f5c9846465a129eca50c19d1c063aaf7
JasonC5 <chenj@migrsoft.com>
2021-09-06 09:08:44 +0800

    每日一题

---
+++ src/com/jason/leetCode/LC704.java
@@ -0,0 +1,33 @@
+package com.jason.leetCode;
+
+/**
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-search
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author JasonC5
+ */
+public class LC704 {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int length = nums.length;
+        int left = 0, right = length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return -1;
+    }
+}
