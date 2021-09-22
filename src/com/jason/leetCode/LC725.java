b42ad0f61a70c550bbce5d2fbe23bafee7c56cf9
JasonC5 <chenj@migrsoft.com>
2021-09-22 09:58:45 +0800

    每日一题

---
+++ src/com/jason/leetCode/LC725.java
@@ -0,0 +1,102 @@
+package com.jason.leetCode;
+
+/**
+ * 725. 分隔链表
+ * 给定一个头结点为 root 的链表, 编写一个函数以将链表分隔为 k 个连续的部分。
+ * <p>
+ * 每部分的长度应该尽可能的相等: 任意两部分的长度差距不能超过 1，也就是说可能有些部分为 null。
+ * <p>
+ * 这k个部分应该按照在链表中出现的顺序进行输出，并且排在前面的部分的长度应该大于或等于后面的长度。
+ * <p>
+ * 返回一个符合上述规则的链表的列表。
+ * <p>
+ * 举例： 1->2->3->4, k = 5 // 5 结果 [ [1], [2], [3], [4], null ]
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入:
+ * root = [1, 2, 3], k = 5
+ * 输出: [[1],[2],[3],[],[]]
+ * 解释:
+ * 输入输出各部分都应该是链表，而不是数组。
+ * 例如, 输入的结点 root 的 val= 1, root.next.val = 2, \root.next.next.val = 3, 且 root.next.next.next = null。
+ * 第一个输出 output[0] 是 output[0].val = 1, output[0].next = null。
+ * 最后一个元素 output[4] 为 null, 它代表了最后一个部分为空链表。
+ * 示例 2：
+ * <p>
+ * 输入:
+ * root = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k = 3
+ * 输出: [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]]
+ * 解释:
+ * 输入被分成了几个连续的部分，并且每部分的长度相差不超过1.前面部分的长度大于等于后面部分的长度。
+ * <p>
+ * <p>
+ * 提示:
+ * <p>
+ * root 的长度范围： [0, 1000].
+ * 输入的每个节点的大小范围：[0, 999].
+ * k 的取值范围： [1, 50].
+ *
+ * @author JasonC5
+ */
+public class LC725 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] ans = new ListNode[k];
+        int length = 0;
+        ListNode reader = head;
+        while (reader != null) {
+            reader = reader.next;
+            length++;
+        }
+        int num = length / k;
+        int moreOne = length % k;
+        reader = head;
+        int count = 0, index = 0;
+        while (reader != null) {
+            if (ans[index] == null) {
+                ans[index] = reader;
+            }
+            count++;
+            ListNode next = reader.next;
+            if ((index < moreOne && count == num + 1) || (index >= moreOne && count == num)) {
+                index++;
+                count = 0;
+                reader.next = null;
+            }
+            reader = next;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = new ListNode(6);
+        head.next.next.next.next.next.next = new ListNode(7);
+        head.next.next.next.next.next.next.next = new ListNode(8);
+        head.next.next.next.next.next.next.next.next = new ListNode(9);
+        head.next.next.next.next.next.next.next.next.next = new ListNode(10);
+        splitListToParts(head, 3);
+    }
+}
