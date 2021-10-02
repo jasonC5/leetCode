4f6358caafdb2a09e148f503b79c2d402395fd58
JasonC5 <chenj@migrsoft.com>
2021-10-02 19:19:51 +0800

    每日一题

--- src/com/jason/leetCode/LC1436.java
+++ src/com/jason/leetCode/LC1436.java
@@ -1,5 +1,9 @@
 package com.jason.leetCode;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *给你一份旅游线路图，该线路图中的旅行线路用数组 paths 表示，其中 paths[i] = [cityAi, cityBi] 表示该线路将会从 cityAi 直接前往 cityBi 。请你找出这次旅行的终点站，即没有任何可以通往其他城市的线路的城市。
  *
@@ -36,4 +40,17 @@ package com.jason.leetCode;
 public class LC1436 {
 
 
+    public String destCity(List<List<String>> paths) {
+        Map<String, String> map = new HashMap<>();
+        for (List<String> path : paths) {
+            map.put(path.get(0), path.get(1));
+        }
+        String city = paths.get(0).get(0);1
+        String next = paths.get(0).get(1);
+        while (next != null) {
+            city = next;
+            next = map.get(next);
+        }
+        return city;
+    }
 }
