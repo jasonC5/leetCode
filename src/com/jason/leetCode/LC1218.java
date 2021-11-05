6325a04f389d20268ef8cb34a47636e230598604
JasonC5 <chenj@migrsoft.com>
2021-11-05 10:05:43 +0800

    每日一题

--- src/com/jason/leetCode/LC1218.java
+++ src/com/jason/leetCode/LC1218.java
@@ -19,4 +19,15 @@ public class LC1218 {
         return max;
     }
 
+    public int longestSubsequence2(int[] arr, int difference) {
+        int[] dp = new int[20000];
+        int max = 0;
+        for (int num : arr) {
+            int length = dp[num - difference + 10000] + 1;
+            max = Math.max(max, length);
+            dp[num + 10000] = length;
+        }
+        return max;
+    }
+
 }
