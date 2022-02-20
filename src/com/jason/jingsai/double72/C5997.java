155ed7d42b7f8062bcbdd852a37a39e969fa1583
JasonC5 <chenj@migrsoft.com>
2022-02-20 15:24:39 +0800

    题解url

--- src/com/jason/jingsai/double72/C5997.java
+++ src/com/jason/jingsai/double72/C5997.java
@@ -1,5 +1,27 @@
 package com.jason.jingsai.double72;
 
+/**
+ * 给你一个整数 num ，请你返回三个连续的整数，它们的 和 为 num 。如果 num 无法被表示成三个连续整数的和，请你返回一个 空 数组。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：num = 33
+ * 输出：[10,11,12]
+ * 解释：33 可以表示为 10 + 11 + 12 = 33 。
+ * 10, 11, 12 是 3 个连续整数，所以返回 [10, 11, 12] 。
+ * 示例 2：
+ *
+ * 输入：num = 4
+ * 输出：[]
+ * 解释：没有办法将 4 表示成 3 个连续整数的和。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-three-consecutive-integers-that-sum-to-a-given-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author JasonC5
+ */
 public class C5997 {
     public long[] sumOfThree(long num) {
         if (num % 3 != 0) {
