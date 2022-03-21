fe94b2be6b0fa8230c5c989d134cf759dab7508d
JasonC5 <chenj@migrsoft.com>
2022-03-21 10:39:57 +0800

    每日一题

---
+++ src/com/jason/leetCode/LC653.java
@@ -0,0 +1,72 @@
+package com.jason.leetCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 给定一个二叉搜索树 root 和一个目标结果 k，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,7], k = 9
+ * 输出: true
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入: root = [5,3,6,2,4,null,7], k = 28
+ * 输出: false
+ *  
+ * <p>
+ * 提示:
+ * <p>
+ * 二叉树的节点个数的范围是  [1, 104].
+ * -104 <= Node.val <= 104
+ * root 为二叉搜索树
+ * -105 <= k <= 105
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author JasonC5
+ */
+public class LC653 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        Set<Integer> exists = new HashSet<>();
+        return traverse(root, k, exists);
+    }
+
+    private boolean traverse(TreeNode node, int k, Set<Integer> exists) {
+        if (node == null) {
+            return false;
+        } else if (exists.contains(k - node.val)) {
+            return true;
+        }
+        exists.add(node.val);
+        return traverse(node.left, k, exists) || traverse(node.right, k, exists);
+    }
+}
