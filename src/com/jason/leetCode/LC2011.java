4d053575fe8b5a32720eae3b0faf7f4f4d9b5b60
chenjieaj <chenjieaj@yonyou.com>
2022-12-23 10:52:59 +0800

    每日一题

--- src/com/jason/leetCode/LC2011.java
+++ src/com/jason/leetCode/LC2011.java
@@ -1,5 +1,7 @@
 package com.jason.leetCode;
 
+import java.util.stream.Stream;
+
 /**
  * @author chenjieaj
  * @date 2022/12/23 9:23:34
@@ -9,17 +11,10 @@ public class LC2011 {
     public int finalValueAfterOperations(String[] operations) {
         int x = 0;
         for (String operation : operations) {
-            char[] chars = operation.toCharArray();
-            for (char c : chars) {
-                if (c == '+'){
-                    x++;
-                    break;
-                } else if (c == '-') {
-                    x--;
-                    break;
-                }
-            }
+            x += operation.charAt(1) == '+' ? 1 : -1;
         }
         return x;
+        // 一行
+//        return Stream.of(operations).mapToInt((s) -> s.charAt(1) == '+' ? 1 : -1).sum();
     }
 }
