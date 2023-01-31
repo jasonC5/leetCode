fb6bd628f91138293f9d23bb9bfc8d8d88a01d84
chenjieaj <chenjieaj@yonyou.com>
2023-01-31 09:23:14 +0800

    每日一题

---
+++ src/com/jason/leetCode/LC2319.java
@@ -0,0 +1,26 @@
+package com.jason.leetCode;
+
+/**
+ * @author chenjieaj
+ * @date 2023/1/31 9:19:11
+ * @description
+ */
+public class LC2319 {
+    public boolean checkXMatrix(int[][] grid) {
+        int n = grid.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j || i + j + 1 == n) {
+                    if (grid[i][j] == 0) {
+                        return false;
+                    }
+                } else {
+                    if (grid[i][j] != 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
