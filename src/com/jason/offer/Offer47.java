9269d030b82decc5998c7cd14962c663011ed428
chenjieaj <chenjieaj@yonyou.com>
2023-03-08 09:40:59 +0800

    每日一题

---
+++ src/com/jason/offer/Offer47.java
@@ -0,0 +1,36 @@
+package com.jason.offer;
+
+/**
+ * @author chenjieaj
+ * @date 2023/3/8 9:30:23
+ * @description
+ */
+public class Offer47 {
+    public static int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = grid[m - 1][n - 1];
+        for (int i = m - 2; i >= 0; i--) {
+            dp[i][n - 1] = grid[i][n - 1] + dp[i + 1][n - 1];
+        }
+        for (int j = n - 2; j >= 0; j--) {
+            dp[m - 1][j] = grid[m - 1][j] + dp[m - 1][j + 1];
+        }
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                dp[i][j] = grid[i][j] + Math.max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{
+                {1, 3, 1},
+                {1, 5, 1},
+                {4, 2, 1}
+        };
+        System.out.println(maxValue(grid));
+    }
+}
