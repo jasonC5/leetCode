b7647a7288d83f90a0a020f49da45e3b1dd84051
chenjieaj <chenjieaj@yonyou.com>
2023-05-15 10:04:14 +0800

    每日一题

---
+++ src/com/jason/leetCode/LC1072.java
@@ -0,0 +1,35 @@
+package com.jason.leetCode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author chenjieaj
+ * @date 2023/5/15 9:49:38
+ * @description
+ */
+public class LC1072 {
+    class Solution {
+        public int maxEqualRowsAfterFlips(int[][] matrix) {
+            int m = matrix.length, n = matrix[0].length;
+            Map<String, Integer> map = new HashMap<String, Integer>();
+            for (int i = 0; i < m; i++) {
+                char[] arr = new char[n];
+                Arrays.fill(arr, '0');
+                for (int j = 0; j < n; j++) {
+                    // 如果 matrix[i][0] 为 1，则对该行元素进行翻转
+                    arr[j] = (char) ('0' + (matrix[i][j] ^ matrix[i][0]));
+                }
+                String s = new String(arr);
+                map.put(s, map.getOrDefault(s, 0) + 1);
+            }
+            int res = 0;
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                res = Math.max(res, entry.getValue());
+            }
+            return res;
+        }
+    }
+
+}
